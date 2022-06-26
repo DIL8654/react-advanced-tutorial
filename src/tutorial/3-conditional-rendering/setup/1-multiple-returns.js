05622c7090eb1ee22c38c37ba286cf427b2097f1
DilankaM <dilanka85@gmail.com>
2022-06-26 11:07:53 +0530

    multiple returns

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
+  return "hello world";
+  // first return everything else will be ignored
   return <h2>multiple returns</h2>;
 };
 
