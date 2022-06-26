51c2cbe596c61952e63e79a6dfff39b7754b78c5
DilankaM <dilanka85@gmail.com>
2022-06-26 11:10:11 +0530

    multiple returns with condition

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  return "hello world";
+  const [loading, setLoading] = useState(false);
+
+  if (loading) {
+    return "loading..";
+  }
   // first return everything else will be ignored
   return <h2>multiple returns</h2>;
 };
